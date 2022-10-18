d316960a76307669e57b5d9ce62733fa305ef3e5
Abdalrahman Shayah <abdalrahmanshayah99@gmail.com>
2022-10-18 23:31:14 +0300

    main

---
+++ include/form.php
@@ -0,0 +1,8 @@
+<?php
+  if(isset($_POST['submit'])){
+    $subject = $_POST["subject"];
+    $description = $_POST["description"];
+    $date = $_POST["date"];
+    $submit = $_POST["submit"];
+  }
+?>
