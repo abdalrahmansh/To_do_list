8b112370d830d85fa41dc14890f13143883d71cb
AbdulRahman <39411019+POI911@users.noreply.github.com>
2022-10-22 04:17:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,84 @@
 <?php 
-  include "./include/form.php";
-  include "./include/modal.php";
+    
 
-  $conn = mysqli_connect('localhost','root','','todo');
-  if(!$conn)
-    echo 'ERROR' . mysqli_connect_error();
-  $sql = 'SELECT * FROM task';
-  $res = mysqli_query($conn,$sql);
-  $users = mysqli_fetch_all($res,MYSQLI_ASSOC);
-  
+	// connect to database
+    // 
+	$db = mysqli_connect("localhost", "root", "", "todoApp");
 
-  
+	// if input is empty, print an error
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			echo 'You have to enter a task';
+		}
+        else{
 
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: index.php');
+		}
+	}
+    
+    
+    // Delete a task if the delete button is clicked
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+    
+        mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+        header('location: index.php');
+    }
+    
+    ?>
 
-  
-
-?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>To do list</title>
-  <link href="/CSS/style.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <!-- JavaScript Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<title>ToDo List Application PHP and MySQL</title>
 </head>
 <body>
+	<div>
+		<h2>To-Do List</h2>
+	</div>
+	<form method="post" action="index.php">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn">Add Task</button>
+	</form>
 
 
- 
+     <!-- displaying the tasks in a table -->
+    <table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Tasks</th>
+			<th >Action</th>
+		</tr>
+	</thead>
+   
+	<tbody>
+		<?php 
+		// select all tasks 
+		$tasks = mysqli_query($db, "SELECT * FROM tasks");
 
-  
+		$i = 1;
+         while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td> <?php echo $row['task']; ?> </td>
+				<td> 
+					<a href="index.php?del_task=<?php echo $row['ID'] ?>">DELETE</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
+
+
+    
+	
+
 </body>
 </html>
+
